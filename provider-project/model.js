540ff7b093494a0254f7f3529e304cebc09b6945
Ryanbenac33 <benacryan1@gmail.com>
2023-08-15 00:02:02 -0500

    correctly configure project api request and translation

--- provider-project/model.js
+++ provider-project/model.js
@@ -11,28 +11,34 @@
 
 //const request = require('request').defaults({ json: true })
 const fetch = import('node-fetch') // requesting from API
-const config = require('../config/default.json')
-const _ = require('../node_modules/lodash') // dealing with arrays and numbers
-const crossFetch = require('../node_modules/cross-fetch') // fetch function fix for node js
+const config = require('config')
+const _ = require('lodash') // dealing with arrays and numbers
+const crossFetch = require('cross-fetch') // fetch function fix for node js
+const {writeFile, readFile} = require("fs")
 
 const tkn = require("../getToken/tokenFunctions")
 
 // throw error if request variables not defined
-if (!config.ogcconnector.instanceID) throw new Error(`ERROR: Instance ID must be defined in your config.`)
-if (!config.ogcconnector.keynetixCloud) throw new Error(`ERROR: Keynetix Cloud Instance must be defined in your config.`)
-if (!config.ogcconnector.contentType) throw new Error(`ERROR: Content type must be defined in your config.`)
-if (!config.ogcconnector.token) throw new Error(`ERROR: Token must be defined in your config.`)
-if (!config.ogcconnector.url) throw new Error(`ERROR: API access URL must be defined in your config.`)
-
-// pull all the config.json data
-const instanceID = config.ogcconnector.instanceID
-const keynetixCloud = config.ogcconnector.keynetixCloud
-const contentType = config.ogcconnector.contentType
-const token = config.ogcconnector.token
-const url = config.ogcconnector.url
-const port = config.ogcconnector.port
+tkn.configCheck()
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // 1. Token Authorization
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // define requestURL
+  const requestURL = config.ogcconnector.request_url
+
+  // encode body with URL form encoding
+  reqBody = tkn.encode(tkn.getBody())
+
+  // call getToken to request new access token from Bentley API
+  tkn.getToken(requestURL, reqBody).then(response => {newToken = response
+    // overwrite existing config token
+    const jsonPath = "./config/default.json"
+    tkn.writeToken(jsonPath, newToken)
+  })
+
+  // then set token for API access
+  var token = config.ogcconnector.token 
 
 
 function opengroundcloud (koop) {}
@@ -47,31 +53,18 @@ opengroundcloud.prototype.getData = function getData (req, callback) {
   // get the host and id
   const { host, id } = req.params
 
-  //url "id" paramenter must be in the form projectID::modelName
-  const details = req.params.id.split('::')
-  const project = details[0]
-  const output = details[1]
-
-  // throw error if id in the wrong format
-  if (!project || !output) callback(new Error('The "id" parameter in the URL must be of form "projects::output"'))
-  if (project !== "projects") callback(new Error('The first "id" parameter in the URL must be "projects"'))
+  // throw error if id not projects
+  if (req.params.id !== "projects") callback(new Error('The first "id" parameter in the URL must be "projects"'))
   
-  // 1. declare API headers
-  const apiHeaders = {
-    "KeynetixCloud": keynetixCloud,
-    "Authorization": `Bearer ${token}`,
-    "Content-Type": contentType,
-    "InstanceId": instanceID
-  }
+  // 2. Declare API data request headers
+  const apiHeaders = tkn.getHeaders(token)
 
   // 2. Construct the OpenGroundCloud API request URLs
+  const url = config.ogcconnector.url
   const requrlOne = `${url}/data/projects`
-  //console.log(`\nAPI URL request: ${requrlOne}\n`)
-
-  console.log(requrlOne)
   
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //use Promise.all to get all needed data at one time to be merged and processed together
+  //use Promise.all to get all needed data at one time to be processed
   Promise.all([
   crossFetch.fetch(requrlOne, {method: "GET", headers: apiHeaders}).then(projects => { 
     if (!projects.ok) {
@@ -79,19 +72,23 @@ opengroundcloud.prototype.getData = function getData (req, callback) {
       const statusText = projects.statusText
       throw new Error(`Request to ${requrlOne} failed - verify you have a current access token; ${status}, ${statusText}.`)
     }
-    //console.log(`${projects.status} Successful Connection to ${requrlOne}`)
 
     return projects.json()
   })]).then(([projects]) => {
     // 3. merge the tables together here and return a table with only the complete data
     const fixedProjets = fixProjectInfo(projects)
 
-    // 5. Create Metadata
-    const geometryType = _.get(fixedProjets, 'features[0].geometry.type', 'Point')
-    fixedProjets.metadata = { geometryType }
+    const geojson = translate(fixedProjets)
 
-    // 6. Fire callback to provider with formatted data
-    callback(null, fixedProjets)
+    // 6. Create Metadata
+    //const geometryType = _.get(geojson, 'features[0].geometry.type', 'Point')
+    geojson['metadata'] = { 
+      geometryType: 'Point',
+      description: 'OpenGround Cloud Boring Data'
+     }
+    
+    // 7. Fire callback to provider with formatted data
+    callback(null, geojson)
 
   }).catch(callback)
 }
@@ -115,20 +112,37 @@ function fixProjectInfo (json) {
     delete currentObj['Group']
     delete currentObj['HasDocuments']
 
-    // add testing URL
-    currentObj['TestingURL'] = `http://localhost:${port}/opengroundcloud/rest/services/${currentObj['Id']}::LocationDetails/FeatureServer/0/query`
   }
-  
   return json
 }
 
-// helper function to create feature class from API input (data, no name table)
+// helper function to create feature class from API project data input
+
 function translate (input) {
   //console.log('Made it to translation')
   return {
     type: 'FeatureCollection',
     features: input.map(formatFeature)
   }
+}
+
+// helper function to create feature class from API project data input
+function formatFeature (inputFeature) {
+  // Most of what we need to do here is extract the longitude and latitude
+  const coords = [0,0]
+
+  // create feature for feature class
+  const feature = {
+    type: "Feature",
+    properties: inputFeature,
+    geometry: {
+      type: "Point",
+      // long,lat
+      coordinates: [Number(coords[0]), Number(coords[1])]
+    }
+  }
+
+  return feature
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
